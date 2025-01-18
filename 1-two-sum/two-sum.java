469cce65430f8fdb0686b09bf090e3b8acfe4b66
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2025-01-18 18:05:56 +0530

    Time: 57 ms (6.17%) | Memory: 44.6 MB (91.70%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,40 +1,61 @@
-class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        // Create an array of custom data class to store values and their original indices
-        Data[] array = new Data[nums.length];
-        
-        // Fill the array with values and their corresponding indices
-        for (int i = 0; i < nums.length; i++) {
-            array[i] = new Data(nums[i], i);
+public class Solution {
+
+    // Helper class to store value and index pair
+    static class IndexedNum {
+        int value;
+        int index;
+
+        IndexedNum(int value, int index) {
+            this.value = value;
+            this.index = index;
         }
-        
-        // Sort the array based on the value
-        Arrays.sort(array, (a, b) -> Integer.compare(a.val, b.val));
-        
-        int i = 0, j = array.length - 1;
-        while (i < j) {
-            int sum = array[i].val + array[j].val;
-            if (sum == target) {
-                // Return the indices of the original positions
-                return new int[] { array[i].idx, array[j].idx };
-            } else if (sum < target) {
-                i++;
-            } else {
-                j--;
+    }
+
+    // Method to compare two IndexedNum objects for sorting
+    static int compare(IndexedNum a, IndexedNum b) {
+        return a.value - b.value;
+    }
+
+    // The twoSum method
+    public static int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        // Create an array of IndexedNum to hold values and their original indices
+        IndexedNum[] indexedNums = new IndexedNum[n];
+        for (int i = 0; i < n; i++) {
+            indexedNums[i] = new IndexedNum(nums[i], i);
+        }
+
+        // Sort the indexedNums array by the value using bubble sort
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (compare(indexedNums[i], indexedNums[j]) > 0) {
+                    // Swap
+                    IndexedNum temp = indexedNums[i];
+                    indexedNums[i] = indexedNums[j];
+                    indexedNums[j] = temp;
+                }
             }
         }
-        
-        return null;
-    }
-    
-    // Data class to store value and its index
-    static class Data {
-        int val;
-        int idx;
-        
-        Data(int val, int idx) {
-            this.val = val;
-            this.idx = idx;
+
+        // Use two pointers to find the target sum
+        int left = 0;
+        int right = n - 1;
+
+        while (left < right) {
+            int sum = indexedNums[left].value + indexedNums[right].value;
+            
+            if (sum == target) {
+                // Return the indices of the numbers
+                return new int[] { indexedNums[left].index, indexedNums[right].index };
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
         }
+
+        // Return an empty array if no solution is found
+        return new int[0];
     }
 }
