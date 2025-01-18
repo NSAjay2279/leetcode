9aebf0816c6d416769d5ccf67c0883d003ef3952
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2025-01-18 16:31:36 +0530

    Time: 8 ms (51.93%) | Memory: 45 MB (58.11%) - LeetSync

--- 1-two-sum/two-sum.java
+++ 1-two-sum/two-sum.java
@@ -1,66 +1,40 @@
-class Solution { 
+class Solution {
     public int[] twoSum(int[] nums, int target) {
-        // Custom hash map to simulate the hash table structure
-        class HashTable {
-            private class Entry {
-                int key;
-                int value;
-                Entry next;
-
-                Entry(int key, int value) {
-                    this.key = key;
-                    this.value = value;
-                    this.next = null;
-                }
-            }
-
-            private Entry[] table;
-
-            public HashTable(int size) {
-                table = new Entry[size];
-            }
-
-            public void put(int key, int value) {
-                // Ensure index is non-negative by taking absolute value
-                int index = Math.abs(key % table.length);
-                Entry newEntry = new Entry(key, value);
-                if (table[index] == null) {
-                    table[index] = newEntry;
-                } else {
-                    Entry current = table[index];
-                    while (current.next != null) {
-                        current = current.next;
-                    }
-                    current.next = newEntry;
-                }
-            }
-
-            public Integer get(int key) {
-                // Ensure index is non-negative by taking absolute value
-                int index = Math.abs(key % table.length);
-                Entry current = table[index];
-                while (current != null) {
-                    if (current.key == key) {
-                        return current.value;
-                    }
-                    current = current.next;
-                }
-                return null;
-            }
-        }
-
-        HashTable hashTable = new HashTable(10000);  // Initialize hash table with a size of 10000
-        int[] result = new int[2];
+        // Create an array of custom data class to store values and their original indices
+        Data[] array = new Data[nums.length];
+        
+        // Fill the array with values and their corresponding indices
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            Integer index = hashTable.get(complement);
-            if (index != null) {
-                result[0] = index;
-                result[1] = i;
-                return result;
-            }
-            hashTable.put(nums[i], i);
+            array[i] = new Data(nums[i], i);
         }
-        return new int[0];  // Return an empty array if no solution is found
+        
+        // Sort the array based on the value
+        Arrays.sort(array, (a, b) -> Integer.compare(a.val, b.val));
+        
+        int i = 0, j = array.length - 1;
+        while (i < j) {
+            int sum = array[i].val + array[j].val;
+            if (sum == target) {
+                // Return the indices of the original positions
+                return new int[] { array[i].idx, array[j].idx };
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        
+        return null;
+    }
+    
+    // Data class to store value and its index
+    static class Data {
+        int val;
+        int idx;
+        
+        Data(int val, int idx) {
+            this.val = val;
+            this.idx = idx;
+        }
     }
 }
