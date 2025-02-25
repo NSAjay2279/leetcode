33709254b2ede88619d1ff022493f133eadb991f
NSAjay2279 <144517147+NSAjay2279@users.noreply.github.com>
2025-02-25 13:24:50 +0530

    Time: 3 ms (58.53%) | Memory: 17.7 MB (73.59%) - LeetSync

---
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.py
@@ -0,0 +1,8 @@
+class Solution:
+    def areOccurrencesEqual(self, s: str) -> bool:
+        freq = {}  
+        
+        for char in s:
+            freq[char] = freq.get(char, 0) + 1  
+        
+        return len(set(freq.values())) == 1
